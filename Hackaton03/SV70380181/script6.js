2993dab78b10b1ef1345b00173cacbeba86d7041
omar <omarprograma72@gmail.com>
2023-10-02 22:18:30 -0500

    av2

---
+++ Hackaton03/SV70380181/script6.js
@@ -0,0 +1,1 @@
+//6. Hacer un algoritmo en JavaScript para ayudar a un trabajador a saber cuál será su sueldo semanal, se sabe que si trabaja 40 horas o menos, se le pagará $20 por hora, pero si trabaja más de 40 horas entonces las horas extras se le pagarán a $25 por hora.
