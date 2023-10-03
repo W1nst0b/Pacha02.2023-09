fb1b5f275c959f196f3710bceb98d5aaaf04988f
omar <omarprograma72@gmail.com>
2023-10-02 23:09:02 -0500

    fin

--- Hackaton03/SV70380181/script18.js
+++ Hackaton03/SV70380181/script18.js
@@ -5,3 +5,33 @@
 //$7. Entre 100 y 499 unidades.
 //$6. Para mas de 500 unidades.
 //La ganancia para el vendedor es de 8,25 % de la venta. Realizar un algoritmo en JavaScript que dado un número de CDs a vender calcule el precio total para el cliente y la ganancia para el vendedor.
+
+function calcularPrecioYGanancia(cantidadCDs) {
+    let precioUnitario = 0;
+    let gananciaVendedor = 0;
+  
+    if (cantidadCDs >= 1 && cantidadCDs <= 9) {
+      precioUnitario = 10;
+    } else if (cantidadCDs >= 10 && cantidadCDs <= 99) {
+      precioUnitario = 8;
+    } else if (cantidadCDs >= 100 && cantidadCDs <= 499) {
+      precioUnitario = 7;
+    } else if (cantidadCDs >= 500) {
+      precioUnitario = 6;
+    } else {
+      return { precioTotal: 0, gananciaVendedor: 0 }; 
+    }
+  
+    const precioTotal = cantidadCDs * precioUnitario;
+    gananciaVendedor = precioTotal * 0.0825; 
+  
+    return { precioTotal, gananciaVendedor };
+  }
+  
+  const cantidadCDs = parseInt(prompt('Ingrese la cantidad de CDs vendidos:'));
+  
+  const { precioTotal, gananciaVendedor } = calcularPrecioYGanancia(cantidadCDs);
+ 
+  console.log(`Precio total para el cliente: $${precioTotal.toFixed(2)}`);
+  console.log(`Ganancia para el vendedor: $${gananciaVendedor.toFixed(2)}`);
+  
--- Hackaton03/SV70380181/script7.js
+++ Hackaton03/SV70380181/script7.js
@@ -3,3 +3,34 @@
 //Tipo A 10% de descuento
 //Tipo B 15% de descuento
 //Tipo C 20% de descuento
+
+function calcularPrecioConDescuento(precioOriginal, tipoMembresia)
+ {
+    let descuento = 0;
+  
+    switch (tipoMembresia)
+     {
+      case 'A':
+        descuento = 0.1; // 10% de descuento para tipo A
+        break;
+      case 'B':
+        descuento = 0.15; // 15% de descuento para tipo B
+        break;
+      case 'C':
+        descuento = 0.2; // 20% de descuento para tipo C
+        break;
+      default:
+        descuento = 0; 
+    }
+  
+    const precioConDescuento = precioOriginal * (1 - descuento);
+    return precioConDescuento;
+  }
+  
+  const precioOriginal = parseFloat(prompt('Ingrese el precio original del helado:'));
+  const tipoMembresia = prompt('Ingrese el tipo de membresía (A, B o C):');
+  
+  const precioConDescuento = calcularPrecioConDescuento(precioOriginal, tipoMembresia);
+  
+  console.log(`El precio con descuento para la membresía ${tipoMembresia} es: $${precioConDescuento.toFixed(2)}`);
+  
